5b5672e372adbc0c961caa4894ae46f1c5f9b6ad
Kristofer <kris@youngers.org>
2026-05-02 12:36:49 -0400

    console echo troubles cleaned up.

--- canal/domains/picoceci/console.go
+++ canal/domains/picoceci/console.go
@@ -10,13 +10,17 @@ import (
 
 // console provides line-based I/O for the REPL.
 // Uses machine.Serial which is USB CDC on ESP32-S3.
+//
+// NOTE: Echo is handled by the terminal (miniterm with local echo) or
+// Canal's serial layer. This console does NOT echo to avoid double characters.
 type console struct{}
 
 func newConsole() *console {
 	return &console{}
 }
 
-// ReadLine reads a line from the serial console with echo and backspace support.
+// ReadLine reads a line from the serial console with local echo.
+// Miniterm doesn't echo by default, so picoceci handles it.
 func (c *console) ReadLine() (string, error) {
 	var sb strings.Builder
 
@@ -31,13 +35,22 @@ func (c *console) ReadLine() (string, error) {
 			return sb.String(), err
 		}
 
-		// Echo the character
-		machine.Serial.WriteByte(b)
-
 		// Handle newline
 		if b == '\n' || b == '\r' {
+			// Echo newline
+			machine.Serial.WriteByte('\r')
+			machine.Serial.WriteByte('\n')
+			// If we got \r, consume any following \n (handles \r\n sequence)
 			if b == '\r' {
-				machine.Serial.WriteByte('\n')
+				time.Sleep(time.Millisecond) // Brief wait for \n to arrive
+				if machine.Serial.Buffered() > 0 {
+					next, _ := machine.Serial.ReadByte()
+					if next != '\n' {
+						// Not a \n, put it back by processing it next iteration
+						// Actually we can't put it back, so just ignore non-\n
+						// This is fine since \r alone is rare
+					}
+				}
 			}
 			break
 		}
@@ -48,17 +61,21 @@ func (c *console) ReadLine() (string, error) {
 			if len(s) > 0 {
 				sb.Reset()
 				sb.WriteString(s[:len(s)-1])
-				// Erase character on terminal: backspace, space, backspace
+				// Erase: move back, overwrite with space, move back
 				machine.Serial.WriteByte(8)
 				machine.Serial.WriteByte(' ')
 				machine.Serial.WriteByte(8)
 			}
+			// Don't echo the backspace character itself
 			continue
 		}
 
 		// Handle Ctrl-C (interrupt)
 		if b == 3 {
-			println("^C")
+			machine.Serial.WriteByte('^')
+			machine.Serial.WriteByte('C')
+			machine.Serial.WriteByte('\r')
+			machine.Serial.WriteByte('\n')
 			sb.Reset()
 			return "", nil
 		}
@@ -68,6 +85,8 @@ func (c *console) ReadLine() (string, error) {
 			return "", &eofError{}
 		}
 
+		// Echo printable characters
+		machine.Serial.WriteByte(b)
 		sb.WriteByte(b)
 	}
 
